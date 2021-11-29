1812a02b0aae27ec9fae9840dc838811a8634581
jonoman55 <jonoman55@yahoo.com>
2021-11-28 20:09:33 -0500

    alt spelling error fixed

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Property from "../components/Property";
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={300} atl={`banner-img-${purpose}`} />
+    <Image src={imageUrl} width={500} height={300} alt={`banner-img-${purpose}`} />
     <Box p="5">
       <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
@@ -29,7 +29,6 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       buttonText="Explore Renting"
       linkName="/search?purpose=for-rent"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-      alt="banner-photo"
     />
     <Flex flexWrap="wrap">
       {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
@@ -43,7 +42,6 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       buttonText="Explore Buying"
       linkName="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-      alt="banner-photo"
     />
     <Flex flexWrap="wrap">
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
