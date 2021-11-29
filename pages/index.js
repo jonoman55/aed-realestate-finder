bef683d3c5bddeaf9d140b5220acf48c430a167c
jonoman55 <jonoman55@yahoo.com>
2021-11-28 20:06:57 -0500

    alt tag added to index

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       buttonText="Explore Renting"
       linkName="/search?purpose=for-rent"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      alt="banner-photo"
     />
     <Flex flexWrap="wrap">
       {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
@@ -42,6 +43,7 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
       buttonText="Explore Buying"
       linkName="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      alt="banner-photo"
     />
     <Flex flexWrap="wrap">
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
